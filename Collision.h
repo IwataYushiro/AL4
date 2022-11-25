4f01ca10762730b47a605eafeea0d008d66f2efe
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-11-25 15:46:10 +0900

    球と平面の当たり判定機能作成

--- Collision.h
+++ Collision.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "CollisionPrimitive.h"
+
+//当たり判定ヘルパー
+class Collision
+{
+public:
+	/// <summary>
+	/// 球と平面の当たり判定
+	/// </summary>
+	/// <param name="sphere">球</param>
+	/// <param name="plane">平面</param>
+	/// <param name="inter">交点(平面上の最近接点)</param>
+	/// <returns>交差しているか否か</returns>
+	static bool ChackSphere2Plane(const Sphere& sphere, const Plane& plane,
+		DirectX::XMVECTOR* inter = nullptr);
+
+private:
+
+};
