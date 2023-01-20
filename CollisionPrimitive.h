366db9387f8159692181d964643bdde7e02993ac
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-01-20 13:57:53 +0900

    球と三角形の当たり判定実装

--- CollisionPrimitive.h
+++ CollisionPrimitive.h
@@ -17,3 +17,14 @@ struct Plane
 	//原点からの座標
 	float distance = 0.0f;
 };
+
+struct Triangle
+{
+	//頂点座標3つ
+	DirectX::XMVECTOR	p0;
+	DirectX::XMVECTOR	p1;
+	DirectX::XMVECTOR	p2;
+
+	//法線ベクトル
+	DirectX::XMVECTOR	normal;
+};
